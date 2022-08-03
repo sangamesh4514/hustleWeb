938761207950470da5469708367b8ad306b5ef9e
sangamesh <sangamesh@convertcart.com>
2022-08-04 01:20:55 +0530

    added base URL

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://hustle4514.herokuapp.com/" });
 
 export const generateOtp = (phoneNumber) =>
   API.get(`/auth/getotp/${phoneNumber}`);
